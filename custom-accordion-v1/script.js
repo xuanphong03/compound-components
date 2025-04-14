234e81b8051f621faf165273fefcfa3ea4781489
Nguyen Xuan Phong <ngxphong03@gmail.com>
2025-04-14 17:25:42 +0700

    fix: toggle custom accordion v1

--- custom-accordion-v1/script.js
+++ custom-accordion-v1/script.js
@@ -12,13 +12,11 @@ class Accordion extends HTMLElement {
   }
   render() {
     this.shadowRoot.innerHTML = `
-      <div class="accordion-wrapper">
-        <slot></slot>
-      </div>
+      <slot></slot>
     `;
   }
   loadStyle() {
-    var style = document.createElement("style");
+    const style = document.createElement("style");
     style.textContent = `
         * {
             margin: 0;
@@ -27,9 +25,9 @@ class Accordion extends HTMLElement {
             border: 0;
             outline: 0;
         }
-        .accordion-wrapper {
+        slot {
+          display:block;
           position: relative;
-          width: 100%;
         }
     `;
     this.shadowRoot.append(style);
@@ -49,10 +47,7 @@ class AccordionItem extends HTMLElement {
     this.addEventToggleAccordion();
   }
   addEventToggleAccordion() {
-    const accordionItemList = document.querySelectorAll(
-      "custom-accordion-item"
-    );
-
+    const accordionItem = this;
     function getInitialHeight(element) {
       // Store original styles
       const originalStyles = {
@@ -65,6 +60,7 @@ class AccordionItem extends HTMLElement {
         visibility: element.style.visibility,
         marginTop: element.style.marginTop,
       };
+
       // Set temporary styles for measurement
       const visibleCSS = {
         position: "absolute",
@@ -79,6 +75,7 @@ class AccordionItem extends HTMLElement {
       // Apply temporary styles to the element
       Object.assign(element.style, visibleCSS);
       const initialElementHeight = element.clientHeight;
+
       // Apply original styles back to the element
       Object.keys(originalStyles).forEach((key) => {
         if (originalStyles[key]) {
@@ -104,61 +101,48 @@ class AccordionItem extends HTMLElement {
       });
     }
 
-    if (accordionItemList && accordionItemList.length) {
-      accordionItemList.forEach(function (accordionItem) {
-        const accordionTrigger = accordionItem.querySelector(
-          "custom-accordion-trigger"
-        );
-        const accordionContent = accordionItem.querySelector(
-          "custom-accordion-content"
-        );
-        if (!accordionTrigger || !accordionContent) return;
-
-        // Set initial height for accordion content
-        const initialAccordionContentStyle = {
-          height: "0px",
-          display: "block",
-          overflow: "hidden",
-          transition: "all 0.3s ease",
-        };
-        Object.assign(accordionContent.style, initialAccordionContentStyle);
-
-        accordionTrigger.addEventListener("click", function (event) {
-          event.preventDefault();
-          if (!accordionContent) return;
-          if (accordionItem.classList.contains("active")) {
-            // Set height to current height before transition
-            accordionContent.style.height =
-              accordionContent.scrollHeight + "px";
-            // Force browser to recognize style change before animation
-            if (accordionContent) {
-              window.getComputedStyle(accordionContent).height;
-            }
-            // Apply new styles to trigger transition
-            accordionItem.classList.remove("active");
-            accordionContent.style.height = "0px";
-          } else {
-            // Close all accordion item
-            closeAllAccordionContent();
-            const contentHeight = getInitialHeight(accordionContent);
-            accordionItem.classList.add("active");
+    if (accordionItem) {
+      const accordionTrigger = accordionItem.querySelector(
+        "custom-accordion-trigger"
+      );
+      const accordionContent = accordionItem.querySelector(
+        "custom-accordion-content"
+      );
+      if (!accordionTrigger || !accordionContent) return;
+      // Add event listener to accordion trigger
+      accordionTrigger.addEventListener("click", function (event) {
+        event.preventDefault();
+        if (!accordionContent) return;
+        console.log(accordionItem.classList.contains("active"));
+        if (accordionItem.classList.contains("active")) {
+          // Set height to current height before transition
+          accordionContent.style.height = accordionContent.scrollHeight + "px";
+          // Force browser to recognize style change before animation
+          if (accordionContent) {
             window.getComputedStyle(accordionContent).height;
-            accordionContent.style.height = contentHeight + "px";
           }
-        });
+          // Apply new styles to trigger transition
+          accordionItem.classList.remove("active");
+          accordionContent.style.height = "0px";
+        } else {
+          // Close all accordion item
+          closeAllAccordionContent();
+          const contentHeight = getInitialHeight(accordionContent);
+          accordionItem.classList.add("active");
+          window.getComputedStyle(accordionContent).height;
+          accordionContent.style.height = contentHeight + "px";
+        }
       });
     }
   }
 
   render() {
     this.shadowRoot.innerHTML = `
-      <div class="accordion-item">
-        <slot></slot>
-      </div>
+      <slot></slot>
     `;
   }
   loadStyle() {
-    var style = document.createElement("style");
+    const style = document.createElement("style");
     style.textContent = `
         * {
             margin: 0;
@@ -168,21 +152,9 @@ class AccordionItem extends HTMLElement {
             outline: 0;
         }
         slot {
-          align-self: stretch;
           display: block;
-          width: 100%;
         }
-        .accordion-item {
-          border: var(--stroke-weight-1, 1px) solid var(--stroke, #dfe1f5);
-          box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-          border-radius: 1rem;
-          backdrop-filter: blur(3px);
-          display: flex;
-          padding: 1.5625rem 1.8125rem 1.5625rem 2.0625rem;
-          flex-direction: column;
-          align-items: flex-start;
-          align-self: stretch;
-        }
+       
     `;
     this.shadowRoot.append(style);
   }
@@ -202,32 +174,14 @@ class AccordionTrigger extends HTMLElement {
 
   render() {
     this.shadowRoot.innerHTML = `
-      <div class="accordion__trigger">
-        <slot></slot>
-        <div class="accordion__trigger-btn">
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="25"
-              viewBox="0 0 24 25"
-              fill="none"
-            >
-              <path
-                d="M12 4.5V20.5M20 12.5H4"
-                stroke="#7584D6"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-          </button>
-        </div>
+      <div class="accordion__trigger-wrapper">
+        <slot>
+        </slot>
       </div>
-    `;
+     `;
   }
   loadStyle() {
-    var style = document.createElement("style");
+    const style = document.createElement("style");
     style.textContent = `
         * {
             margin: 0;
@@ -237,44 +191,15 @@ class AccordionTrigger extends HTMLElement {
             outline: 0;
         }
         slot {
-          display: block;
-        }
-        .accordion__trigger {
-          color: var(--text-title, #004b88);
-          text-align: center;
-          font-family: "Averta Std CY";
-          font-size: 1.125rem;
-          font-style: normal;
-          font-weight: 600;
-          line-height: 150%; 
-          cursor: pointer;
-          align-self: stretch;
           display: flex;
-          align-items: center;
           justify-content: space-between;
-        }
-        .accordion__trigger .accordion__trigger-btn {
-          position: relative;
-          width: 1.5rem;
-          height: 1.5rem;
-        }
-        .accordion__trigger .accordion__trigger-btn button {
-          background: none;
           cursor: pointer;
-          display: inline-block;
-          width: 100%;
-          height: 100%;
-          background: none;
         }
-        .accordion__trigger .accordion__trigger-btn button svg {
-          width: 100%;
-          height: 100%;
-        }
+      
     `;
     this.shadowRoot.append(style);
   }
 }
-
 class AccordionContent extends HTMLElement {
   constructor() {
     super();
@@ -293,7 +218,7 @@ class AccordionContent extends HTMLElement {
     `;
   }
   loadStyle() {
-    var style = document.createElement("style");
+    const style = document.createElement("style");
     style.textContent = `
         * {
             margin: 0;
@@ -311,7 +236,43 @@ class AccordionContent extends HTMLElement {
   }
 }
 
+class AccordionToggleButton extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({
+      mode: "open",
+    });
+  }
+  connectedCallback() {
+    this.render();
+    this.loadStyle();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+      <slot></slot>
+    `;
+  }
+  loadStyle() {
+    const style = document.createElement("style");
+    style.textContent = `
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            border: 0;
+            outline: 0;
+        }
+        slot {
+          position: relative;
+        }
+    `;
+    this.shadowRoot.append(style);
+  }
+}
+
 customElements.define("custom-accordion", Accordion);
 customElements.define("custom-accordion-item", AccordionItem);
 customElements.define("custom-accordion-trigger", AccordionTrigger);
 customElements.define("custom-accordion-content", AccordionContent);
+customElements.define("custom-accordion-toggle-button", AccordionToggleButton);
